cc51c0bd516c8711465cb8875df1c36b7ff80c44
laizhihuan <kojavaee@qq.com>
2015-04-19 18:35:27 +0800

    update touch

---
+++ MyCppGame/Classes/Touch.cpp
@@ -0,0 +1,9 @@
+//
+//  Touch.cpp
+//  MyCppGame
+//
+//  Created by zhihuanglai on 15/4/19.
+//
+//
+
+#include <stdio.h>
---
+++ MyCppGame/Classes/Touch.h
@@ -0,0 +1,8 @@
+//
+//  Touch.h
+//  MyCppGame
+//
+//  Created by zhihuanglai on 15/4/19.
+//
+//
+
