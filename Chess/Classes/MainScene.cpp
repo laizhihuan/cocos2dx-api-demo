34a8cd4a8dc6e41284f7630df738f24cf5b2bb1f
laizhihuan <kojavaee@qq.com>
2015-05-12 14:10:01 +0800

    add touch event

--- Chess/Classes/MainScene.cpp
+++ Chess/Classes/MainScene.cpp
@@ -15,14 +15,43 @@ bool MainScene::init()
     
     int driff = 100;
     
+    // 创建红棋
     Sprite* sprite1 = Sprite::create("bkg2.png");
 	addChild(sprite1);
 	sprite1->setPosition(ccp(winSize.width/2 - driff, winSize.height / 2));
     
+    // 创建黑棋
     Sprite* sprite2 = Sprite::create("bkg1.png");
     addChild(sprite2);
     sprite2->setPosition(ccp(winSize.width/2 + driff, winSize.height/2));
     
+    setTouchEnabled(true);
+    setTouchMode(kCCTouchesOneByOne);
+    
     return true;
+}
+
+bool MainScene::onTouchBegan(Touch *touch, Event *unused_event)
+{
+    return true;
+}
+
+void MainScene::onTouchMoved(Touch *touch, Event *unused_event)
+{
     
 }
+
+void MainScene::onTouchEnded(Touch *touch, Event *unused_event)
+{
+    log("click on touch ended");
+}
+
+void MainScene::onTouchCancelled(Touch *touch, Event *unused_event)
+{
+    
+}
+
+void MainScene::update(float delta)
+{
+}
+
--- Chess/Classes/MainScene.h
+++ Chess/Classes/MainScene.h
@@ -17,6 +17,19 @@ class MainScene : public Layer
 public:
     CREATE_FUNC(MainScene);
     bool init();
+    
+    bool onTouchBegan(Touch *touch, Event *unused_event);
+    void onTouchMoved(Touch *touch, Event *unused_event);
+    void onTouchEnded(Touch *touch, Event *unused_event);
+    void onTouchCancelled(Touch *touch, Event *unused_event);
+    
+    Sprite* _red;    //红方
+    Sprite* _black;  //黑方
+    
+    bool _selected;  //选择了那一方
+    
+    void update(float delta);  //帧循环
+    
 };
 
 #endif /* defined(__Chess__MainScene__) */
