39a5646f0da5c5a1d2a8ae877c8a33f8caab3d6e
laizhihuan <kojavaee@qq.com>
2015-05-22 18:49:49 +0800

    add ai

---
+++ Chess/Classes/AI.cpp
@@ -0,0 +1,9 @@
+//
+//  AI.cpp
+//  Chess
+//
+//  Created by zhihuanglai on 15/5/22.
+//
+//
+
+#include "AI.h"
---
+++ Chess/Classes/AI.h
@@ -0,0 +1,14 @@
+//
+//  AI.h
+//  Chess
+//
+//  Created by zhihuanglai on 15/5/22.
+//
+//
+
+#ifndef __Chess__AI__
+#define __Chess__AI__
+
+#include <iostream>
+
+#endif /* defined(__Chess__AI__) */
