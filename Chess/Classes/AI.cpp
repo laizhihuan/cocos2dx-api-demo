00f6b8167842f58ecdff297de384e3939773a265
laizhihuan <kojavaee@qq.com>
2015-06-09 09:00:47 +0800

    add api impl

--- Chess/Classes/AI.cpp
+++ Chess/Classes/AI.cpp
@@ -7,3 +7,28 @@
 //
 
 #include "AI.h"
+
+ccArray* AI::allPosisibleMove(SceneGame* game)
+{
+    return NULL;
+}
+
+
+int AI::calcScore(SceneGame* game)
+{
+    return 0;
+}
+
+Step* AI::genStep(SceneGame* game)
+{
+    return NULL;
+}
+
+void AI::fakeMove(SceneGame* game, Step* step)
+{
+}
+
+void AI::unfakeMove(SceneGame* game, Step* step)
+{
+    
+}
