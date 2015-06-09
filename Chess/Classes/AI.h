3b94d2b3e356c43982ee0597660a5ec8680ac704
laizhihuan <kojavaee@qq.com>
2015-06-09 08:55:25 +0800

    add remark

--- Chess/Classes/AI.h
+++ Chess/Classes/AI.h
@@ -15,15 +15,43 @@
 class AI
 {
 public:
-    
-    Step* genStep(Game* game);
-    
+    /**
+     *  生成ai的步骤
+     *
+     *  @param game 游戏逻辑
+     *
+     *  @return ai的棋子下一步
+     */
+    Step* genStep(SceneGame* game);
+    /**
+     *  所有可能的走法
+     *
+     *  @param game <#game description#>
+     *
+     *  @return <#return value description#>
+     */
     ccArray* allPosisibleMove(SceneGame* game);
-    
+    /**
+     *  模拟移动棋子
+     *
+     *  @param game <#game description#>
+     *  @param step <#step description#>
+     */
     void fakeMove(SceneGame* game, Step* step);
-    
+    /**
+     *  撤销模拟移动
+     *
+     *  @param game <#game description#>
+     *  @param step <#step description#>
+     */
     void unfakeMove(SceneGame* game, Step* step);
-    
+    /**
+     *  计算分数
+     *
+     *  @param game <#game description#>
+     *
+     *  @return <#return value description#>
+     */
     int calcScore(SceneGame* game);
     
 };
