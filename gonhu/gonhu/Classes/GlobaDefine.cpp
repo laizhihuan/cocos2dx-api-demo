879c69720dceb58c0d9bd59fee4d684c2e5697f2
laizhihuan <kojavaee@qq.com>
2015-06-15 09:22:44 +0800

    add globe define

---
+++ gonhu/gonhu/Classes/GlobaDefine.cpp
@@ -0,0 +1,9 @@
+//
+//  GlobaDefine.cpp
+//  gonhu
+//
+//  Created by zhihuanglai on 15/6/15.
+//
+//
+
+#include "GlobaDefine.h"
---
+++ gonhu/gonhu/Classes/GlobaDefine.h
@@ -0,0 +1,17 @@
+//
+//  GlobaDefine.h
+//  gonhu，游戏中用到的全局定义（公共函数）
+//
+//  Created by zhihuanglai on 15/6/15.
+//
+//
+
+#ifndef __gonhu__GlobaDefine__
+#define __gonhu__GlobaDefine__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+#define setIntToXML UserDefault::getInstance()->setIntegerForKey
+
+#endif /* defined(__gonhu__GlobaDefine__) */
