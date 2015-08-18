ff3f6697c676bb04ce687fc2c6a14d823bb48697
laizhihuan <kojavaee@qq.com>
2015-08-18 08:27:55 +0800

    add net imply

--- Chess/Classes/Net.h
+++ Chess/Classes/Net.h
@@ -9,6 +9,39 @@
 #ifndef __Chess__Net__
 #define __Chess__Net__
 
-#include <iostream>
+#define SOCKET int
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+#define closesocket close
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Net
+{
+protected:
+    Net()
+    {
+        _server = INVALID_SOCKET;
+        _client = INVALID_SOCKET;
+        _isServer = false;
+        _isListen = false;
+        _isConnect = false;
+    }
+    
+    SOCKET _server;
+    SOCKET _client;
+    
+    bool _isServer;
+    bool _isListen;
+    bool _isConnect;
+    static Net* _theOne;
+};
 
 #endif /* defined(__Chess__Net__) */
